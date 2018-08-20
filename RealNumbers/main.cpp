763445f931ee3dd9643af1b3519d901eba974eba
blacerda <blacerda@mail.ccsf.edu>
2018-08-20 12:04:09 -0700

    instantiating a basic class using real numbers as an example part II

--- RealNumbers/main.cpp
+++ RealNumbers/main.cpp
@@ -4,12 +4,27 @@ int main() {
 
     //creating a variable of this class
     //an object named c
-    ComplexNumber c;//call the no argument constructor
+    ComplexNumber c1, c2;//call the no argument constructor
 
-    c.print();
+    c1.print();//calling print member function using dot operator
+    cout<<"c1 holds "<<endl;
+    c2.print();//calling print member function using dot operator
+    cout<<"c2 holds"<<endl;
+
+    //updating c1
+    c1.setMemberVariables(3.14, 5.3);
     cout<<endl;
-    c.setMemberVariables(3.14, 5.3);
-    cout<<endl;
-    c.print();
+    c1.print();
     cout<<"done"<<endl;
+
+    //updating c2
+    c2.setMemberVariables(4, 5.3);
+    cout<<endl;
+    c2.print();
+    cout<<"done"<<endl;
+
+
+
+
+
 }
