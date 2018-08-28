0c684d52d19eefd0a75007e8bb0f1850dc213ced
blacerda <blacerda@mail.ccsf.edu>
2018-08-28 11:27:45 -0700

    returning objects from functions

--- NewOperator/main.cpp
+++ NewOperator/main.cpp
@@ -34,10 +34,24 @@ public:
     }
 };
 
-
+/*
+ *
+ * Returning Objects from functions
+ * we can create a function that just creates an object
+ * we are actually creating a temporary object in this function
+ * so this calls a copy constructor and then it is called again in main
+ * making this ineficient if we do not use pointers
+ */
+Animal *createAnimal() {
+    //new allocates memory on the heap (bigger)
+    Animal pAnimal = new Animal();
+    pAnimal->setName("Bertie");
+    return pAnimal;
+}
 int main() {
     //the new operator allocates memory for a single object
     Animal *pCat1 = new Animal();
+
     //we can't call a setter on a pointer, only on an object
     //so we will have to to dereference syntax on pCat1
     //if we did it like this: *pCat1.setName("Freddy"); the dot operator would have higher precedence
@@ -45,10 +59,23 @@ int main() {
     //we could do this : (*pCat1).setName("Freddy"); and in this case we could use regular dot syntax
     //but, it is ugly, hence the use of ->
     //to use new we always have to use delete after to deallocate memory
+    //we can't call delete on null pointers
     pCat1->speak();
     delete pCat1;
 
     cout << sizeof(pCat1) << endl;
+/*
+ *
+ * Returning Objects from functions
+ * using pointers we won't be making a copy constructor in main anymore
+ * only in the createAnimal() function
+ * we are only passing a pointer around saving memory
+ *
+ */
+    Animal *pFrog = createAnimal();
+    pFrog->speak();
+
+    delete pFrog;
 
     return 0;
 }
